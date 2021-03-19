79cbd2f1b25ec1a11b5d1a73d74f8b3c7df49de8
suzanemuza123 <72234636+suzanemuza123@users.noreply.github.com>
2021-03-19 16:03:12 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 /* As a user,I want to be able to:
 -type in text
 -click submit
@@ -11,10 +13,22 @@ From the technical side:
 -display data accordingly
 */
 $(document).ready(()=> {
+    
   $("#submit").click(()=> {
   let userInput=$("#search").val()
   alert(userInput)
+  $.ajax({
+
+    url: "https://developers.giphy.com/api=8dNpSXbJaCtgq3yx2PXWroLjWrYRBE6r=${userInput}"
   
-.done(function (){
-$(this).addclass("done");
+.done((res)=>{
+    let star = res,Search;
+    $.each(star, (i, e)=>{
+        let star = e.star
+        console.log("star", star)
+        $("body").append('<img src="${star}" alt="stars"/>')
+    }
+    
+}
+
   
